90690506e3b376a675cad1d501d373ee131cdd7f
Luiz Carlos Mourão Paes de Carvalho <luizcarlosmpc@gmail.com>
2020-08-11 01:14:29 -0300

    Added dotenv for debug environments

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,22 @@
+mod logger;
+
+#[macro_use]
+extern crate log;
+
+#[cfg(debug_assertions)]
+use dotenv::dotenv;
+
+use std::env;
+
 fn main() {
+    #[cfg(debug_assertions)]
+    dotenv().ok();
+
+    let amqp_host = env::var("AMQP_HOST");
+
+    if let Ok(value) = amqp_host {
+        println!("{}", value);        
+    } else {
+        return
+    }
 }
