2a432b9d51f72890222b717f5acfcddc085a508a
saiintbrisson <luizcarlosmpc@gmail.com>
2020-08-16 00:42:28 -0300

    Created JsonEmail

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,33 +1,29 @@
-use lettre::smtp::authentication::{Credentials, Mechanism};
-use lettre::smtp::client::net::ClientTlsParameters;
+use lettre::{
+    smtp::{
+        authentication::{Credentials, Mechanism},
+        client::net::ClientTlsParameters,
+        ConnectionReuseParameters, response::Response
+    },
+    Transport, ClientSecurity, 
+    SmtpClient, SmtpTransport
+};
 
-use lettre::smtp::{ConnectionReuseParameters, response::Response};
+use lettre_email::{Email, error::Error};
 
-use lettre::{Transport, SmtpClient, ClientSecurity, SmtpTransport};
+use std::{sync::Mutex, collections::HashMap};
 
-use lettre_email::Email;
-
-use std::sync::Mutex;
+use std::fs::{read_dir, read_to_string};
 
 lazy_static! {
 
     static ref MAILER: Mutex<SmtpTransport> = {
-        use std::env::var;
-
-        let address = var("SMTP_SERVER").unwrap_or_else(|_| {
-            warn!("Could not find SMTP_SERVER variable, falling back to development server");
-            "smtp.mailtrap.io".into()
-        });
+        use crate::CONFIG;
+        
+        let address = &CONFIG.smtp_server;
 
         let credentials = Credentials::new(
-            var("SMTP_USERNAME").unwrap_or_else(|_| {
-                warn!("Could not find SMTP_USERNAME variable, falling back to default username");
-                "username".into()
-            }), 
-            var("SMTP_PASSWORD").unwrap_or_else(|_| {
-                warn!("Could not find SMTP_PASSWORD variable, falling back to default password");
-                "password".into()
-            })
+            (&CONFIG.smtp_username).clone(), 
+            (&CONFIG.smtp_password).clone()
         );
 
         use native_tls::{Protocol, TlsConnector};
@@ -42,7 +38,7 @@ lazy_static! {
         );
 
         Mutex::new(
-            SmtpClient::new((address.as_str(), 465), security)
+            SmtpClient::new((address.as_str(), 587), security)
                 .unwrap()
                 .smtp_utf8(true)
                 .credentials(credentials)
@@ -52,14 +48,41 @@ lazy_static! {
         )
     };
 
+    static ref TEMPLATES_MAP: HashMap<String, String> = {
+        let mut map = HashMap::new();
+
+        let dir = read_dir("assets/templates/").expect("Could not list template dir entries");
+
+        dir.filter(| entry | entry.is_ok())
+            .map(| entry | entry.unwrap())
+            .for_each(| entry | {
+                let name = entry.file_name().into_string();
+    
+                if name.is_err() {
+                    return
+                }
+    
+                if let Ok(value) = read_to_string(entry.path()) {
+                    let name = name.unwrap();
+                    let name = name.chars().take(name.len() - 5).collect::<String>();
+
+                    map.insert(name, value);
+                }
+            });
+
+        map
+    };
+
 }
 
-pub fn send_mail(body: String) -> Result<Response, Box<dyn std::error::Error>> {
-    let email = Email::builder()
-            .from("no-reply@wckd.store")
-            .to("customer@domain.tld")
-            .body(body)
-            .build()?;
+pub fn list_templates() {
+    TEMPLATES_MAP.keys().for_each(| key | {
+        info!("Known template: {}", key);
+    });
+}
+
+pub fn send_mail(email: JsonEmail) -> Result<Response, Box<dyn std::error::Error>> {
+    let email = email.to_sendable_email()?;
 
     let mut mailer = MAILER.lock().unwrap();
 
@@ -68,4 +91,81 @@ pub fn send_mail(body: String) -> Result<Response, Box<dyn std::error::Error>> {
     mailer.close();
 
     Ok(result)
+}
+
+use std::io::{Error as IoError, ErrorKind};
+
+use serde_json::{from_slice, Result as SerdeResult};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct JsonEmail {
+
+    pub template: String,
+
+    pub from: String,
+    pub to: Vec<String>,
+
+    pub subject: String,
+
+    pub body: Option<String>,
+    pub placeholders: Option<HashMap<String, String>>
+
+}
+
+impl JsonEmail {
+
+    pub fn from_slice(json: &[u8]) -> SerdeResult<Self> {
+        from_slice(json)
+    }
+
+    pub fn to_sendable_email(&self) -> Result<Email, Error> {
+        let mut builder = Email::builder()
+                                        .from(self.from.as_str())
+                                        .subject(self.subject.as_str());
+
+        for addr in self.to.iter() {
+            builder = builder.to(addr.as_str());
+        }
+
+        let template = self.template.to_lowercase();
+
+        if template == "custom" {
+            match &self.body {
+                Some(body) => builder = builder.html(body.as_str()),
+                None => return create_error("Invalid body")
+            }
+
+            return builder.build()
+        }
+
+        match TEMPLATES_MAP.get(&template) {
+            Some(body) => {
+                match &self.placeholders {
+                    Some(placeholders) => {
+                        let mut body = body.clone();
+
+                        placeholders.iter()
+                                    .for_each(| (key, value) | {
+                                        body = body.replace(key, value);
+                                    });
+                        
+                        builder = builder.html(body);
+                    },
+
+                    None => return create_error("Invalid placeholders")
+                }
+                
+                builder = builder.html(body.as_str());
+            },
+
+            None => return create_error("Invalid template")
+        }
+
+        builder.build()
+    }
+
+}
+
+fn create_error(error: &str) -> Result<Email, Error> {
+    Err(Error::Io(IoError::new(ErrorKind::NotFound, error)))
 }
